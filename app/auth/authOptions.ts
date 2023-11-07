278fe738b8791e6306f51249be6ba50b4fda2233
Georges Parapanissios <georgesparapanissios@Georgess-MBP.station>
2023-11-07 09:09:33 +0200

    add auth firebase-adapter

--- app/auth/authOptions.ts
+++ app/auth/authOptions.ts
@@ -1,7 +1,18 @@
 import GoogleProvider from 'next-auth/providers/google';
 import { NextAuthOptions } from 'next-auth';
+import { FirestoreAdapter } from '@auth/firebase-adapter';
+import { cert } from 'firebase-admin/app';
+
+const { privateKey } = JSON.parse(process.env.FIREBASE_PRIVATE_KEY!);
 
 const authOptions: NextAuthOptions = {
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey,
+    }),
+  }),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
--- firebase/auth.ts
+++
@@ -1,7 +0,0 @@
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import app from './app';
-
-const provider = new GoogleAuthProvider();
-const auth = getAuth(app);
-
-export { auth, provider };
